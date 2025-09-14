6d7713d9ef9c81d3a79b7cc3ed198de0ffdad2ae
Anumk.aip10 <Anumk.aip10@gmail.com>
2025-09-14 21:32:48 +0500

    practice of Excercise

---
+++ Excercise5.py
@@ -0,0 +1,8 @@
+# Ask the user for the day of the week(like monday)if its saturday or sunday print"weekend", else "Weekday"
+a = str(input("Enter today day: ")).lower()
+
+Currentyear = (2025)
+if a == "saturday" or a == "sunday" :
+    print("Weekend")
+else:
+    print("Weekday")
