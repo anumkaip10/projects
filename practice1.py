073cc3182481797dbbc6d0a099979071bcb6d596
Anumk.aip10 <Anumk.aip10@gmail.com>
2025-09-16 21:13:35 +0500

    practice of lectures

---
+++ practice1.py
@@ -0,0 +1,11 @@
+i = 0
+while(i<3):
+    print(i)
+    i = i + 1
+while (i<3):
+    print(i)
+    i = i + 2
+i = int(input("Enter the number: "))
+while(i<38):
+    i = int(input("Enter the number: "))
+    print (i)
