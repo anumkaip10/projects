fc3bd24eea512bce13a1d806a7869f1784d597ab
Anumk.aip10 <Anumk.aip10@gmail.com>
2025-09-16 21:53:25 +0500

    practice of lecture

---
+++ practice2.py
@@ -0,0 +1,26 @@
+def calculategemean(a,b):
+ mean = (a*b)/(a+b)
+ print(mean)
+def isGreater(a,b):
+ Calculategemean = (a,b)
+ if(a>b):
+  print("First number is greater")
+ else:
+  print("Second number is greater or equal")
+  def islesser(a,b):
+   pass
+ a = 9
+ b = 8
+ isGreater = (a,b)
+# gemean1 = (a*b)/(a+b)
+# print(gemean1)
+c = 8
+d = 74
+isGreater = (c, d)
+if(c>d):
+ print("First is greater number")
+else:
+ print("Second is greater number")
+calculategemean = (c,d)
+# gemean2 = (c*d)/(c+d)
+# print(gemean2)
