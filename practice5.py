cc901dcfaa1400e12abe41bb9fa03f5e9e438aac
Anumk.aip10 <Anumk.aip10@gmail.com>
2025-09-18 21:12:20 +0500

    practice of daily lessons

---
+++ practice5.py
@@ -0,0 +1,11 @@
+tup = (1, 5, 6)
+print(type(tup),tup)
+print(tup[0])
+print(tup[1])
+print(tup[2])
+
+if 5 in tup:
+    print("Yes, 5 is present in this tuple")
+    
+tup3= tup [1:3]
+print("tuple3")
