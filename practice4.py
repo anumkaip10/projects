2249c5b625dc2577fa47d691ef55ec3b488a2aa9
Anumk.aip10 <Anumk.aip10@gmail.com>
2025-09-18 20:53:08 +0500

    practice of daily lessions and lectures

---
+++ practice4.py
@@ -0,0 +1,17 @@
+l = [11, 45, 1, 3, 4, 5, 8, 13]
+print(l)
+# l.append(7)
+# l.sort(7)
+# l.sort(7)
+# l.reverse(7)
+# print(l.index(1))
+# print(l.count(1))
+# print(l.copy(1))
+# m = 1
+# m[0]=0
+# print(1)
+m = [900,1000,1100]
+k = l + m
+print(k)
+# l.extend(m)
+print(l)
