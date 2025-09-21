4a2b9cc0e9ec8dffb14e5767a3eada847b487f9f
Anumk.aip10 <Anumk.aip10@gmail.com>
2025-09-21 20:00:54 +0500

    practice of daily lessons for portfolio

---
+++ practice14.py
@@ -0,0 +1,12 @@
+ep = {122: 45, 123: 89, 567: 69, 670: 69}
+ep2 = {1222: 67, 566: 90}
+
+# ep.update(ep2)
+print(ep)
+# ep.update(ep2)
+# ep.pop(122)
+ep.clear()
+print(ep)
+empt = {}
+del ep
+print(ep2)
