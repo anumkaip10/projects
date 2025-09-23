4bb3cb955f2e6a2608a22b56ec5760776d0dc954
Anumk.aip10 <Anumk.aip10@gmail.com>
2025-09-23 21:43:10 +0500

    mini quiz for practice of given lesson

---
+++ miniquiz.py
@@ -0,0 +1,7 @@
+# try to create an empty set, check using the type() function weather the type of variable is a set.
+Anum = set()
+print(type(Anum))
+if("check using the type(), if variable"):
+    print("The empty set is a variable")
+else:
+    print("The empty set is not a variable")
