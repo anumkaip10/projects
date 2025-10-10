20d32e6a4781fcd4de9a143f6b1086b8e47f304c
Anumk.aip10 <Anumk.aip10@gmail.com>
2025-10-10 21:27:44 +0500

    practice of usual excercises

---
+++ Excercise24.py
@@ -0,0 +1,9 @@
+# Detective’s Notebook (List Practice), Create a list of 5 suspects and print the 2nd and last one.
+list["suspects":"Amna, Ali, Akira, Fatima, Ahmed"]
+print("Amna was in kitchen, making herself sandwhich")
+print("Ali was playing fooball outside")
+print("Akira was in her room, studing")
+print("Ahmed was in his grandma's house, he came by drawn")
+print("Fatima had blood in her black heels")
+print("Shes the main suspect i think.")
+
