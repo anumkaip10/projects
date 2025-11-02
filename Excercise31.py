965230a1999053c700d8e7675356d03f52fbc42e
Anumk.aip10 <Anumk.aip10@gmail.com>
2025-11-02 16:50:23 +0500

    practice of important lessons

---
+++ Excercise31.py
@@ -0,0 +1,15 @@
+with open('myfile12.txt', 'r') as f:
+    lines = f.readlines()
+
+if not lines:
+    print("File is empty or missing!")
+else:
+    for i, line in enumerate(lines, start=1):
+        parts = line.strip().split(',')
+        if len(parts) == 3:
+            m1, m2, m3 = parts
+            print(f"marks of student {i} in Maths is: {m1}")
+            print(f"marks of student {i} in English is: {m2}")
+            print(f"marks of student {i} in Science is: {m3}")
+        else:
+            print(f"⚠️ Line {i} format is wrong: {line}")
