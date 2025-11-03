8f77ee3d8ce6f040d7ff5d2a1267dcb92a514944
Anumk.aip10 <Anumk.aip10@gmail.com>
2025-11-03 17:49:44 +0500

    CONSTRUCTORS IN PYTHON

---
+++ Excercise46.py
@@ -0,0 +1,21 @@
+class person:
+ def __init__(self, n, o):
+    print("hey i am a person")
+
+    self.name = n
+    self.occ = o
+
+    name = "AK"
+    occ = "AI Engineer"
+
+def info(self):
+    print(f"{self.name} is a {self.acc}")
+a = person("AK", "AI Engineer")
+b = person("Amna", "HR")
+c = person(1, 2)
+a.info()
+b.info()
+c.info()
+# print (a.name)
+# a.name = "Amna"
+# a.acc = "HR"
